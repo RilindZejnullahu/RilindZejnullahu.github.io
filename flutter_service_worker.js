e856223a737408f47d9f851e4768adbe42d4309e
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2021-02-16 19:07:50 +0100

    testing markers

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,17 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "96583e769631bb6658a1304ba0483bb1",
+"assets/assets/data/stops.txt": "23af4a604d7297b6e4c97ea3f90dce3d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "02f7fb60402bb33a54d799881fd84ecc",
+"assets/NOTICES": "bf973dd0243ad97f3865924bdd3767fa",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6a5c6a26b71485ced64912508beefb32",
-"/": "6a5c6a26b71485ced64912508beefb32",
-"main.dart.js": "fc7e047780e34bcfdc94686cc1499486",
+"index.html": "55bee02991255dba5c9b6620da30e46b",
+"/": "55bee02991255dba5c9b6620da30e46b",
+"main.dart.js": "d1a9696ac66a31d635a3455cd10de2d6",
 "manifest.json": "c5892224b8638f883387765acbd1da04",
 "version.json": "7d01db3330c0f81fcf135a6d0a07b1f7"
 };
