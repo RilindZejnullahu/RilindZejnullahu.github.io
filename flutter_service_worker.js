d83313f2fa869e046c415e252bd731549de97a83
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2021-03-29 16:03:57 +0200

    Newest test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b4bbcbb2df757b3b773f561c9c266b7e",
-"/": "b4bbcbb2df757b3b773f561c9c266b7e",
-"main.dart.js": "c66bf42d9a8cdba448ac5729ca91990f",
+"index.html": "05d3bac0e9b78b1a70590f5bbeb84a34",
+"/": "05d3bac0e9b78b1a70590f5bbeb84a34",
+"main.dart.js": "4128d8a0374f7896d51bc4d798baf76e",
 "manifest.json": "c5892224b8638f883387765acbd1da04",
 "version.json": "7d01db3330c0f81fcf135a6d0a07b1f7"
 };
