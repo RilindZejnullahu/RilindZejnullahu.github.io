78a737690a269eb97c50045b9f46005e76f25d1c
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2021-04-23 04:35:33 +0200

    change datepicker to swedish

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,11 @@ let config = {};
 self.addEventListener('install', function (e) {
     console.debug('[ServiceWorker] Installing offline worker');
     e.waitUntil(
-        fetch("./package_4056910c73e7375e9fe03d037e5fe800086a239c/uno-config.js")
+        fetch("./package_85d3c6e99ee09f807d6eb9c637e05eeef0373a13/uno-config.js")
             .then(r => r.text()
                 .then(configStr => {
                     eval(configStr);
-                    caches.open('package_4056910c73e7375e9fe03d037e5fe800086a239c').then(function (cache) {
+                    caches.open('package_85d3c6e99ee09f807d6eb9c637e05eeef0373a13').then(function (cache) {
                         console.debug('[ServiceWorker] Caching app binaries and content');
                         return cache.addAll(config.offline_files);
                     });
