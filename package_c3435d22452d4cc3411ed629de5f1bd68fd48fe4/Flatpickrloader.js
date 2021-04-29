cdb80783e90c290a2106f5d402059a7d34c83b3c
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2021-04-29 22:12:32 +0200

    flutter

--- package_c3435d22452d4cc3411ed629de5f1bd68fd48fe4/Flatpickrloader.js
+++
@@ -1,9 +0,0 @@
-﻿﻿(function () {
-    const head = document.getElementsByTagName("head")[0];
-
-    // Load Flatpickr CSS from CDN
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = "https://npmcdn.com/flatpickr/dist/themes/material_blue.css";
-    head.appendChild(link);
-})();
