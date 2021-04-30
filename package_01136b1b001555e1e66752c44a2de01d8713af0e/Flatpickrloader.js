ab80e5a19b4a4730f38356ec5ec8f3c64f2f3475
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2021-04-30 14:01:21 +0200

    latest

---
+++ package_01136b1b001555e1e66752c44a2de01d8713af0e/Flatpickrloader.js
@@ -0,0 +1,9 @@
+﻿﻿(function () {
+    const head = document.getElementsByTagName("head")[0];
+
+    // Load Flatpickr CSS from CDN
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://npmcdn.com/flatpickr/dist/themes/material_blue.css";
+    head.appendChild(link);
+})();
---
+++ package_01136b1b001555e1e66752c44a2de01d8713af0e/GetCurrentTime.js
@@ -0,0 +1,4 @@
+﻿function javaTime() {
+    var date = new Date();
+    return date.toLocaleTimeString().substring(0, 5);
+}
